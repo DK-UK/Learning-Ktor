bb69587f585f9dcd3e3e7c85a9af2b32006aa002
dhavalkeni.enjay <dhavalkeni.enjay@gmail.com>
2022-12-25 11:17:23 +0530

    show list of articles, specific articles

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,14 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.http.content.*
+import com.example.models.Article
+import com.example.models.Article.Companion.articles
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.http.content.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     
@@ -15,6 +18,41 @@ fun Application.configureRouting() {
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
             resources("files")
+        }
+        get("/") {
+            call.respondRedirect("articles")
+        }
+        route("/articles"){
+            get{
+                // for all articles
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to articles)))
+            }
+            get("new"){
+                // Show a page with fields for creating a new article
+                call.respond(FreeMarkerContent("new.ftl", null))
+            }
+            post {
+                // to save article
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val article = Article.newEntry(title, body)
+                articles.add(article)
+                call.respondRedirect("/articles/${article.id}")
+            }
+            get("{id}") {
+                // Show an article with a specific id
+                val id = call.parameters.getOrFail("id").toInt()
+                val article = articles.find { it.id == id }
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to mutableListOf(article))))
+            }
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+            }
+            post("{id}") {
+                // Update or delete an article
+            }
+
         }
     }
 }
